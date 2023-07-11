d88c15a714ecade6a4e5254f9fad3b38b89fe7de
matthewhou19 <80615048+matthewhou19@users.noreply.github.com>
2023-07-11 12:14:05 -0700

    backend

---
+++ backend/gpt-agents/src/test/java/com/example/gptagents/GptAgentsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gptagents;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GptAgentsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
--- gptAgentsFE/src/app/chat-element/chat-element.component.ts
+++ gptAgentsFE/src/app/chat-element/chat-element.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input } from '@angular/core';
 import { Chat } from '../modle/chat';
 import { AgentsService } from '../services/agents/agents.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-chat-element',
@@ -11,5 +12,8 @@ export class ChatElementComponent {
   @Input()
   chat: Chat;
 
-  constructor() {}
+  constructor(private router: Router) {}
+  openChat() {
+    this.router.navigate(['chat-list/chat-board/' + this.chat.id]);
+  }
 }
