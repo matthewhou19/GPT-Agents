c1e607142354b2e8f06c240cd9ed5ec51a1c5db9
matthewhou19 <80615048+matthewhou19@users.noreply.github.com>
2023-08-16 19:16:53 -0700

    backend unit test config

--- backend/gpt-agents/src/test/java/com/example/gptagents/GptAgentsApplicationTests.java
+++ backend/gpt-agents/src/test/java/com/example/gptagents/GptAgentsApplicationTests.java
@@ -2,8 +2,10 @@ package com.example.gptagents;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class GptAgentsApplicationTests {
 
 	@Test
---
+++ backend/gpt-agents/src/test/java/com/example/gptagents/unit/AgentControllerTest.java
@@ -0,0 +1,4 @@
+package com.example.gptagents.unit;
+
+public class AgentControllerTest {
+}
